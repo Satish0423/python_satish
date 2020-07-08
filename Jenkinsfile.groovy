22536c3d230e426542a6eec4901352786abb9420
sai satish <saisatish.inala@gmail.com>
2020-07-08 12:45:57 +0530

    init commit

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,36 @@
+pipeline {
+    agent any 
+    stages {
+        stage('clone repository') {
+            steps {
+                script {
+                    sh """
+                    git clone git@github.com:Satish0423/python_satish.git
+                    cd python_satish
+                    git checkout feature/test
+                    """
+                }
+            }
+        }
+        stage('code format') {
+            steps {
+                script {
+                    sh """
+                    autopep8 --in-place --aggressive --aggressive python.py
+                    """
+                }
+            }
+        }
+        stage('commit and push to repoitory') {
+            steps {
+                script {
+                    sh """
+                    git add python.py
+                    git commit -m "format using script"
+                    git push
+                    """
+                }
+            }
+        }
+    }
+}

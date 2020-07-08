2b1414caf5368b966762aacc84698770e6be17fb
sai satish <saisatish.inala@gmail.com>
2020-07-08 14:52:37 +0530

    init commit

---
+++ Jenkinsfile_artifact.groovy
@@ -0,0 +1,26 @@
+pipeline {
+    agent none 
+    stages {
+        stage('clone repository') {
+            steps {
+                script {
+                    sh """
+                    git clone git@github.com:Satish0423/python_satish.git
+                    cd python_satish
+                    """
+                }
+            }
+        }
+        stage('compie c file') {
+            steps {
+                script {
+                    sh """
+                    gcc hello.c -Wall -o hello
+                    ./hello
+                    """
+                }
+            }
+        }
+    }
+    archiveartifacts artifacts: 'python_satish/hello'
+}
